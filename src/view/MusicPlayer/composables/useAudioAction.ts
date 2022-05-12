9a0f47afaf57f79d865622b66ca5433f5fbc08ec
huangwentao <huangwentao@dianchu.com>
2022-05-12 23:24:45 +0800

    chore: 修改工具函数的引入问题

--- src/view/MusicPlayer/composables/useAudioAction.ts
+++ src/view/MusicPlayer/composables/useAudioAction.ts
@@ -6,7 +6,6 @@
 import FeastService from "@/service/audio";
 import { audioInfo } from "@/interfaces/audio";
 import { onMounted, ref, watch, computed, reactive, nextTick } from "vue";
-import { formatSeconds } from "@/utils/index";
 export const useAudioAction = () => {
   const audioMenus = ref<Array<audioInfo>[]>([]);
   const audioIndex = ref<number>(0); // 批次
@@ -237,4 +236,23 @@ export const useAudioAction = () => {
     changeProgress,
     changeNextBatch,
   };
+};
+
+/**
+ * 返回给定秒数的 ISO 格式('00:00:00')
+ * @param {num} s : 秒数
+ * @returns '00:00:00'
+ */
+const formatSeconds = (s: number) => {
+  const [hour, minute, second, sign] =
+    s > 0
+      ? [s / 3600, (s / 60) % 60, s % 60, ""]
+      : [-s / 3600, (-s / 60) % 60, -s % 60, "-"];
+
+  return (
+    sign +
+    [hour, minute, second]
+      .map((v) => `${Math.floor(v)}`.padStart(2, "0"))
+      .join(":")
+  );
 };
